97560502b32f54325e4940c3fd74ea4d74886a3e
Martin <alialibakhshi9898@gmail.com>
2025-07-09 08:35:57 +0100

    feat:♻️ Make a test language

--- backend/test/api/unit/test_messgae_codes.py
+++ backend/test/api/unit/test_messgae_codes.py
@@ -7,5 +7,11 @@ from app.utils.message_codes import MessageCodes
 
 
 class TestMessageCodes:
+    
     @pytest.mark.asyncio
-    async def test_accept_language_en(self)->None:...
+    async def test_accept_language_en(self)-> None:
+        test_case = "da, en-gb;q=0.8,"
+        _request_accept_language_var.set(test_case)
+        
+        response_message = MessageCodes.get_message(MessageCodes.successful_operation)
+        assert response_message == MessageCodes.english_messages_names[MessageCodes.successful_operation]
