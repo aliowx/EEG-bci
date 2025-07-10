cbf24adcd20f50c0f69d46b08cb74b8d43d2b88c
Martin <alialibakhshi9898@gmail.com>
2025-07-10 20:02:11 +0100

    feat: 🐛 fix

--- backend/test/api/unit/test_messgae_codes.py
+++ backend/test/api/unit/test_messgae_codes.py
@@ -15,3 +15,12 @@ class TestMessageCodes:
         
         response_message = MessageCodes.get_message(MessageCodes.successful_operation)
         assert response_message == MessageCodes.english_messages_names[MessageCodes.successful_operation]
+        
+    
+    @pytest.mark.asyncio
+    async def test_accept_language_default(self)-> None:
+        test_case = "de; q=1.0, es; q=0.5"
+        _request_accept_language_var.set(test_case)
+        
+        response_message = MessageCodes.get_message(MessageCodes.not_found)
+        assert response_message == MessageCodes.english_messages_names[MessageCodes.not_found]
