b8e47982c25ce228df9cd2ce9a7510ba93c249fd
Martin <alialibakhshi9898@gmail.com>
2025-10-24 14:56:24 +0100

    construction:🚧 nothing to learn

--- notebook/EEG/predictionEEG_with_.cpp
+++ notebook/EEG/predictionEEG_with_.cpp
@@ -1,5 +1,12 @@
-#include <torch/torch.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
+
+using namespace std;
+
+namespace hello_world {
+
+string hello() { return "Goodbye, Mars!"; }
+
+}  // namespace hello_world
